2d65f235d443c74ae23454a9a31700f7b2bcf151
zhouyoujun <a390687273@qq.com>
2023-04-13 19:39:11 +0800

    加入 parcel，将 button 组件改为单文件组件

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Button from './button'
+
+Vue.component('g-button', Button)
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+
+        }
+    }
+})
---
+++ src/button.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <button class="g-button">按钮</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
+
+<style lang="scss">
+.g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+
+    &:hover {
+        border-color: var(--border-color-hover);
+    }
+
+    &:active {
+        background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+        outline: none;
+    }
+
+}
+
+
+
+#app {
+    margin: 20px;
+}
+
+body {
+    font-size: var(--font-size);
+}
+</style>
