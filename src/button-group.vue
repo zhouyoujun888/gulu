a318f485191832b55be82088df60e98b3b18dc51
zhouyoujun <a390687273@qq.com>
2023-11-17 11:34:06 +0800

    在mounted钩子里判断用户输入的子元素是不是button

--- src/button-group.vue
+++ src/button-group.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="button-group">
+  <div class="g-button-group">
     <slot></slot>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase();
+      if (name !== "button") {
+        console.warn(`g-button-group的子元素应该是g-button,你写的是${name}`);
+      }
+    }
+  },
+};
 </script>
 
 <style lang="scss" >
-.button-group {
+.g-button-group {
   display: inline-flex;
   justify-content: center;
   align-items: center;
